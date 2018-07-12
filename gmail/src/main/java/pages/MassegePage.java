d7fc305a56328a981e1ee2913fbf8cd9e0c34f82
DavtyanH <38629857+DavtyanH@users.noreply.github.com>
2018-07-13 00:27:33 +0400

    new1

---
+++ gmail/src/main/java/pages/MassegePage.java
@@ -0,0 +1,14 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+import basePage.PageObject;
+
+public class MassegePage extends PageObject{
+
+	public MassegePage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
